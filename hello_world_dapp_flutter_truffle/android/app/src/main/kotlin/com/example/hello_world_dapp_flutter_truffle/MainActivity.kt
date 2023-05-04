17ea19dc593ee15a3bd6ee9fdaed951013e17032
AwaisHaleem <awaishaleem20@gmail.com>
2023-05-04 15:02:56 +0500

    updated code

--- hello_world_dapp_flutter_truffle/android/app/src/main/kotlin/com/example/hello_world_dapp_flutter_truffle/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hello_world_dapp_flutter_truffle
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- notes dapp flutter truffle/lib/Models/note.dart
+++
@@ -1,6 +0,0 @@
-class Note {
-  final int id;
-  final String title;
-  final String description;
-  Note({required this.id, required this.title, required this.description});
-}
